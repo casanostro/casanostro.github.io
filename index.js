cdcf2c4b10b90e048206ad688a399972fef0c96d
casanostro <adrien.tripon@gmail.com>
2025-05-09 12:02:48 +0200

    Updates

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 var vite_config_default = defineConfig({
-  base: "/casanostro.github.io/",
+  base: "/casanostro.github.io",
   // très important !
   plugins: [
     react(),
